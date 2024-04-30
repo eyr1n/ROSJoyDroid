f339edef0e8ecbb78ce3085b2ae56a35210854cd
Rin Iwai <rin@eyrin.jp>
2024-04-30 09:59:04 +0900

    レイアウト変えた

--- app/src/main/java/jp/eyrin/rosjoydroid/MainActivity.kt
+++ app/src/main/java/jp/eyrin/rosjoydroid/MainActivity.kt
@@ -2,29 +2,26 @@ package jp.eyrin.rosjoydroid
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.LifecycleResumeEffect
 import jp.eyrin.rosjoydroid.ui.theme.ROSJoyDroidTheme
 import java.util.Timer
@@ -34,7 +31,6 @@ import kotlin.math.max
 class MainActivity : GamepadActivity() {
     private lateinit var publishJoyTimer: Timer
 
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val prefs = getPreferences(Context.MODE_PRIVATE)
@@ -58,64 +54,63 @@ class MainActivity : GamepadActivity() {
 
             ROSJoyDroidTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
                 ) {
-                    Scaffold(
-                        topBar = {
-                            TopAppBar(colors = TopAppBarDefaults.topAppBarColors(
-                                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                                titleContentColor = MaterialTheme.colorScheme.primary,
-                            ), title = {
-                                Text("ROSJoyDroid")
-                            })
-                        },
-                    ) { innerPadding ->
-                        Column(
-                            modifier = Modifier
-                                .padding(innerPadding)
-                                .padding(10.dp),
-                            verticalArrangement = Arrangement.spacedBy(20.dp)
-                        ) {
-                            Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
-                                OutlinedTextField(
-                                    label = { Text("Domain ID") },
-                                    value = domainId.toString(),
-                                    onValueChange = {
-                                        domainId = (it.toIntOrNull() ?: 0).coerceIn(0, 101)
-                                    },
-                                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
-                                    singleLine = true,
-                                )
-                                OutlinedTextField(
-                                    label = { Text("Namespace") },
-                                    value = ns,
-                                    onValueChange = {
-                                        ns = it
-                                    },
-                                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                                    singleLine = true,
-                                )
-                            }
-                            Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
-                                OutlinedTextField(
-                                    label = { Text("Period(ms)") },
-                                    value = period.toString(),
-                                    onValueChange = {
-                                        period = max(it.toLongOrNull() ?: 20, 1)
-                                    },
-                                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
-                                    singleLine = true,
-                                )
-                                OutlinedTextField(
-                                    label = { Text("Dead zone") },
-                                    value = deadZone.toString(),
-                                    onValueChange = {
-                                        deadZone = (it.toFloatOrNull() ?: 0.05f).coerceIn(0f, 1f)
-                                    },
-                                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                                    singleLine = true,
-                                )
-                            }
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(
+                            space = 10.dp,
+                            alignment = Alignment.CenterVertically,
+                        )
+                    ) {
+                        Text(
+                            text = "ROSJoyDroid",
+                            fontSize = 32.sp,
+                        )
+                        Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+                            OutlinedTextField(
+                                modifier = Modifier.width(160.dp),
+                                value = domainId.toString(),
+                                onValueChange = {
+                                    domainId = (it.toIntOrNull() ?: 0).coerceIn(0, 101)
+                                },
+                                label = { Text("Domain ID") },
+                                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
+                                singleLine = true,
+                            )
+                            OutlinedTextField(
+                                modifier = Modifier.width(160.dp),
+                                value = ns,
+                                onValueChange = {
+                                    ns = it
+                                },
+                                label = { Text("Namespace") },
+                                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                                singleLine = true,
+                            )
+                        }
+                        Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+                            OutlinedTextField(
+                                modifier = Modifier.width(160.dp),
+                                value = period.toString(),
+                                onValueChange = {
+                                    period = max(it.toLongOrNull() ?: 20, 1)
+                                },
+                                label = { Text("Period (ms)") },
+                                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
+                                singleLine = true,
+                            )
+                            OutlinedTextField(
+                                modifier = Modifier.width(160.dp),
+                                value = deadZone.toString(),
+                                onValueChange = {
+                                    deadZone = (it.toFloatOrNull() ?: 0.05f).coerceIn(0f, 1f)
+                                },
+                                label = { Text("Dead zone") },
+                                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                                singleLine = true,
+                            )
                         }
                     }
                 }
@@ -129,7 +124,6 @@ class MainActivity : GamepadActivity() {
         publishJoyTimer = Timer().also {
             it.schedule(object : TimerTask() {
                 override fun run() {
-                    Log.d("lr", axes.joinToString(","))
                     publishJoy(axes, buttons)
                 }
             }, 0, period)
